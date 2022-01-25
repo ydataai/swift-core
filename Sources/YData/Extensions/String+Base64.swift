29eff08adf208f766621851fbf57cf03501fc45a
Luís Portela Afonso <luis.portela@ydata.ai>
2022-01-25 10:29:33 +0000

    feat(base64): create methods to decode and encode base64 strings

---
+++ Sources/YData/Extensions/String+Base64.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+extension String {
+  var base64Decoded: String? {
+    guard let data = Data(base64Encoded: self) else { return nil }
+    return String(data: data, encoding: .utf8)
+  }
+
+  var base64Encoded: String? {
+    data(using: .utf8)?.base64EncodedString()
+  }
+}
