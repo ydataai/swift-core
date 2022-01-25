57ecf4171806793ca27c2642a427247c9d7091e1
Luís Portela Afonso <luis.portela@ydata.ai>
2022-01-25 10:47:49 +0000

    fix(base64): make extension public 🤦 (#11)

--- Sources/YData/Extensions/String+Base64.swift
+++ Sources/YData/Extensions/String+Base64.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension String {
+public extension String {
   var base64Decoded: String? {
     guard let data = Data(base64Encoded: self) else { return nil }
     return String(data: data, encoding: .utf8)
