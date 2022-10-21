478d9d32d08476c1ec310ea5643c3f852f0622a3
Luís Portela Afonso <luis.portela@ydata.ai>
2022-10-21 16:11:25 +0100

    fix(fabricerror): small fixes found during integration (#28)
    * conform to Equatable
    * convert to struct
    * make init public
    * default value for httpCode and convert to mandatory

--- Sources/YData/Core/Error/FabricError.swift
+++ Sources/YData/Core/Error/FabricError.swift
@@ -1,13 +1,23 @@
 import Foundation
 
-public protocol FabricError: Error, Codable {
+public protocol FabricError: Error, Codable, Equatable {
   var context: [String: String]? { get }
   var description: String { get }
-  var httpCode: Int? { get }
+  var httpCode: Int { get }
   var name: String { get }
   var returnValue: Int { get }
 }
 
 public extension FabricError {
   var name: String { "\(Self.self)" }
+}
+
+public extension FabricError {
+  static func == (lhs: Self, rhs: Self) -> Bool {
+    lhs.context == rhs.context
+    && lhs.description == rhs.description
+    && lhs.httpCode == rhs.httpCode
+    && lhs.name == rhs.name
+    && lhs.returnValue == rhs.returnValue
+  }
 }
